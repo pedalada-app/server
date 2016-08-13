43623b385ac87b44228c9f51c9ec632d45381cd8
Danny <dannyb7878@gmail.com>
2016-08-13 12:14:24 +0300

    fix git push problem

--- db/models/teams.js
+++ db/models/teams.js
@@ -1,3 +1,40 @@
-/**
- * Created by danny on 8/13/16.
- */
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var TeamSchema = new Schema({
+
+
+	api_detail: {
+		id: {
+			type: Number,
+			index: true
+		}
+	},
+
+	name: {
+		type: String
+	},
+
+	shortName: {
+		type: String
+	},
+
+	competitions: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Competition"
+	}],
+
+	fixtures: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Fixtures"
+	}],
+
+	crestUrl: {
+		type: String
+	}
+
+});
+
+
+module.exports = mongoose.model("Team", TeamSchema);
