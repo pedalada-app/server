1420fcd870b249b0eb1686d2016863e0b7f66c9e
yuval1488 <yuval1488@gmail.com>
2016-09-16 12:53:19 +0300

    add config file

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var FacebookTokenStrategy = require('passport-facebook-token');
+var passport = require('passport');
+
+// dealing with facebook login authentication
+passport.use(new FacebookTokenStrategy({
+
+}));
 
 var routes = require('./src/routes/index');
 
 var app = express();
-
 
 app.use(compression());
 app.use(logger('dev'));
@@ -17,14 +23,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -32,23 +39,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
---
+++ backend/config/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+	facebookAuth: {
+		clientID: 'your-secret-clientID-here', // your App ID
+		clientSecret: 'your-client-secret-here' // your App Secret
+	}
+};
